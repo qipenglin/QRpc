ee0f3d6e78b13996b9439b1fa2eeae6cb94a050a
qipenglin <674693651@qq.com>
2020-08-29 21:43:06 +0800

    优化pom配置

---
+++ qrpc-test-server/src/main/java/com/qipeng/qrpc/test/server/web/TestController.java
@@ -0,0 +1,12 @@
+package com.qipeng.qrpc.test.server.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "OK";
+    }
+}
